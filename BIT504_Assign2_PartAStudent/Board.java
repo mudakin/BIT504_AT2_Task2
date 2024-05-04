7e2bac7956cc15814a2d9970176cbddca360e349
user <user@mudakin>
2024-05-04 17:23:40 +1200

    Added code to check if the player has 3 in the playerCol.
    Added code to check if the player has three symbols along a diagonal
    line from top-right to bottom-left.

--- BIT504_Assign2_PartAStudent/Board.java
+++ BIT504_Assign2_PartAStudent/Board.java
@@ -54,6 +54,8 @@ public class Board {
 		
 		 // TODO: Check if the player has 3 in the playerCol.
 		 // Hint: Use the row code above as a starting point, remember that it goes cells[row][column] 
+		if (cells[0][playerCol].content == thePlayer && cells[1][playerCol].content == thePlayer && cells[2][playerCol].content == thePlayer)
+			return true;
 		
 		
 		
@@ -63,6 +65,8 @@ public class Board {
 		 
 		
 		// TODO: Check the diagonal in the other direction
+		if (cells[0][2].content == thePlayer && cells[1][1].content == thePlayer && cells[2][0].content == thePlayer)
+            return true;
 		
 
 		
